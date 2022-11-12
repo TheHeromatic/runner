148e626d20f3493914cfa5c7f51ee9bb7e9c8b5c
TheHeromatic <moritzfcb2105@gmail.com>
2022-11-12 16:11:32 +0100

    settings update + "leave game" option

--- src/components/GameScreen.vue
+++ src/components/GameScreen.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
+  <router-link to="/">
+    <button class="buttonHomeScreen">leave game</button>
+  </router-link>
   <div id="field"> <div id="player"></div>
   </div>
 </template>
--- src/components/SettingsScreen.vue
+++ src/components/SettingsScreen.vue
@@ -1,14 +1,16 @@
 <template>
+  <div class="homeScreenDiv">
+    <h1 class="settingsTitle">Settings</h1>
+  </div>
 
+  <button class="buttonDarkMode">Dark Mode</button>
+  <button class="buttonLightMode">Light Mode</button>
+  <br>
   <router-link to="/">
-    <button class="buttonHomeScreen">Back to home</button>
-  </router-link>
+  <button class="buttonHomeScreen">Back to Menu</button>
+</router-link>
 </template>
 
 <script setup>
 
 </script>
-
-<style>
-
-</style>
