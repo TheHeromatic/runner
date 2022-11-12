514a123a9bd3cfc6972fda8e23ae511ad8017fa5
TheHeromatic <moritzfcb2105@gmail.com>
2022-11-12 14:36:18 +0100

    Update GameScreen.vue

--- src/components/GameScreen.vue
+++ src/components/GameScreen.vue
@@ -1,11 +1,25 @@
-<template>
-<div> hello w</div>
-</template>
-
 <script setup>
-
 </script>
 
-<style scoped>
+<template>
+  <div id="field"> <div id="player"></div>
+  </div>
+</template>
 
+<style>
+#field {
+  background-color: greenyellow;
+  height: 900px;
+  width: 680px;
+  position: relative;
+  border: solid 5px black;
+}
+#player {
+  background-color: orangered;
+  height: 40px;
+  width: 40px;
+  position: absolute;
+  border: solid 3px black;
+  z-index: 1000;
+}
 </style>
